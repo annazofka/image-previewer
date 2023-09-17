1b39b0f1c57f52e5d5f1c34e50f65725d7c16429
Kocia <annazofka@gmail.com>
2023-09-17 22:54:18 +0100

    first version of the lightbox

--- 010-projekt/script.js
+++ 010-projekt/script.js
@@ -1,39 +1,52 @@
-let zmienna = 10;
+/*  
+Zdjecia (assety) zostaly nazwne numerycznie od 0 +
 
-zrobCosWaznego();
+1. Po kliknieciu na zdjecie, otworz backdrop z podgladem zdjecia ktore zostalo klikniete, przyciskami oraz x
 
-function zrobCosWaznego() {
-  zmienna = 20;
+2. Po kliknieciu w X, chcemy zeby backdrop z pogladem zniknal
 
-  setTimeout(() => {
-    console.log("Coś Ważnego");
-  }, 1000);
+3. Po kliknieci w next button, chcemy aby przegladane zdjecia sie inkrementowaly 
+
+4. Po kliknieci w prev button, chcemy aby przegladane zdjecia sie dekremenotowaly
+    a) obsluz sytuacje gdy mamy zdjecie zerowe. Jezeli wartosc countera bedzie zero, nie mozemy dekrementowac jej ponizej
+    b) obsluz sytuacje na maksymalna liczbe zdjec
+*/
+
+const $imageList = document.querySelector('#image-list')
+const $closeButton = document.querySelector('#close-button')
+const $prevButton = document.querySelector('#prev-button')
+const $nextButton = document.querySelector('#next-button')
+const $backdrop = document.querySelector('#backdrop')
+const $imagePreviewer = document.querySelector('#image-previewer')
+let counter = 0
+
+const assignImageSource = imageId => {
+  $imagePreviewer.src = `/assets/${imageId}.jpg`
 }
 
-const funckja1 = () => {
-  console.log("funckja 1");
-};
+$imageList.addEventListener('click', event => {
+	if (event.target.localName === 'img') {
+		const imageId = event.target.id
+		counter = imageId * 1
+    assignImageSource(imageId)
+		$backdrop.classList.remove('hide')
+	}
+})
 
-const funckja2 = () => {
-  console.log("funckja 2");
-};
+$closeButton.addEventListener('click', () => {
+	$backdrop.classList.add('hide')
+})
 
-const addTwoNumbers = (a, b) => {
-  console.log(a);
-  console.log(b);
-  return a + b;
-};
+$prevButton.addEventListener('click', () => {
+	if (counter > 0) {
+		counter = counter - 1
+		assignImageSource(counter)
+	}
+})
 
-const init = () => {
-  zrobCosWaznego();
-
-  funckja1();
-
-  funckja2();
-
-  const wynikDodawnia = addTwoNumbers(2, 3);
-
-  console.log(wynikDodawnia);
-};
-
-init();
+$nextButton.addEventListener('click', () => {
+	if (counter < 2) {
+		counter = counter + 1
+		assignImageSource(counter)
+	}
+})
